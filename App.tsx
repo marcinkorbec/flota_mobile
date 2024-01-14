7b719abb0646493320b034693285a3aa35b2260d
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2024-01-14 23:48:51 +0100

    Add navigation and screens to App component

--- App.tsx
+++ App.tsx
@@ -1,23 +1,54 @@
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SplashScreen from './components/SplashScreen/SplashScreen';
+import LoginScreen from './components/LoginScreen/LoginScreen';
+import HomeScreen from './components/HomeScreen/HomeScreen';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#E8364F', // ustawienie koloru tła nagłówka na czerwony
+                },
+                headerTintColor: '#fff', // ustawienie koloru elementów nagłówka (np. tytułu, przycisków) na biały
+                headerTitleAlign: 'center'
+            }}
+        >
+            <Stack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ title: 'Moje tankowania' }} />
+        </Stack.Navigator>
+    );
+}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Witaj Marcinie właśnie zacząłeś przygode z React Native hehe</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const MyTheme = {
+        ...DefaultTheme,
+        colors: {
+            ...DefaultTheme.colors,
+            primary: '#E8364F',
+        },
+    };
+    return (
+        <NavigationContainer >
+            <MyStack />
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'dodgerblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingLeft: 20,
-    paddingRight: 20,
-    border: '1x solid red',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'dodgerblue',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingLeft: 20,
+        paddingRight: 20,
+        border: '1x solid red',
+    },
 });
