667e5937fe39f3b57547b16cd180d8d142d8bf3f
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2024-01-14 23:51:32 +0100

    Add navigation types

---
+++ types/navigation-types.ts
@@ -0,0 +1,11 @@
+import { StackNavigationProp } from '@react-navigation/stack';
+
+// typy dla stosu nawigacji
+export type RootStackParamList = {
+    SplashScreen: undefined;
+    LoginScreen: undefined;
+    HomeScreen: undefined;
+};
+
+export type SplashScreenNavigationProp = StackNavigationProp<RootStackParamList, 'LoginScreen'>;
+export type LoginScreenNavigationProp = StackNavigationProp<RootStackParamList, 'HomeScreen'>;
