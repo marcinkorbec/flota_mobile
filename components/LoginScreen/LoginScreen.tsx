b2e9c5dcd00a361fe4ccf7f9436b76a043cff328
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2024-01-15 02:03:58 +0100

    Add StatusBar to LoginScreen component

--- components/LoginScreen/LoginScreen.tsx
+++ components/LoginScreen/LoginScreen.tsx
@@ -1,7 +1,7 @@
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet, Text, TouchableOpacity, ImageBackground } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Text, TouchableOpacity, ImageBackground, StatusBar } from 'react-native';
 import { RootStackParamList } from '../../types/navigation-types';
 
 type SplashScreenProps = {
@@ -19,30 +19,32 @@ const LoginScreen: React.FC<SplashScreenProps> = ({ navigation }) => {
     };
 
     return (
-        <ImageBackground
-            source={require('../../assets/Screenshot_9.png')}
-            style={styles.container}
-        >
-            <View style={styles.container}>
-                <TextInput
-                    style={styles.input}
-                    placeholder="Podaj numer rejestracyjny pojazdu"
-                    value={username}
-                    onChangeText={setUsername}
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder="Hasło - pin do karty paliwowej"
-                    value={password}
-                    onChangeText={setPassword}
-                    secureTextEntry
-                />
-                <TouchableOpacity style={styles.button} onPress={handleLogin}>
-                    <Text style={styles.buttonText}>Zaloguj się</Text>
-                </TouchableOpacity>
-            </View>
-        </ImageBackground>
-
+        <>
+            <StatusBar barStyle="dark-content" backgroundColor="transparent" />
+            <ImageBackground
+                source={require('../../assets/Screenshot_9.png')}
+                style={styles.container}
+            >
+                <View style={styles.container}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Podaj numer rejestracyjny pojazdu"
+                        value={username}
+                        onChangeText={setUsername}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Hasło - pin do karty paliwowej"
+                        value={password}
+                        onChangeText={setPassword}
+                        secureTextEntry
+                    />
+                    <TouchableOpacity style={styles.button} onPress={handleLogin}>
+                        <Text style={styles.buttonText}>Zaloguj się</Text>
+                    </TouchableOpacity>
+                </View>
+            </ImageBackground>
+        </>
     );
 };
 
