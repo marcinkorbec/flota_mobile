3da870d24e0d05ef8b8cd54bffc080ce3c589a00
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2024-01-28 01:39:40 +0100

    Add responsive width to button container

--- components/HomeScreen/HomeScreen.tsx
+++ components/HomeScreen/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, TouchableOpacity, Text, StyleSheet, StatusBar } from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet, StatusBar, Dimensions } from 'react-native';
 import { useNavigation } from '@react-navigation/native'; // Import the useNavigation hook
 import Ionicons from 'react-native-vector-icons/Ionicons'; // Import the Ionicons component
 import { FontAwesome } from '@expo/vector-icons';
@@ -9,6 +9,15 @@ import { RootStackParamList } from '../../types/navigation-types';
 import { MenuModal } from '../Menu/MenuModal';
 
 type NavigationProp = StackNavigationProp<RootStackParamList, 'HomeScreen'>;
+
+const buttons = [
+    { screen: 'AddFuelScreen', icon: 'gas-pump', text: 'Dodaj Tankowanie', iconSet: FontAwesome5 },
+    { screen: 'AddRoadFeesScreen', icon: 'road', text: 'Dodaj koszt drogowy', iconSet: FontAwesome },
+    { screen: 'AddDriverCostsScreen', icon: 'money-bill', text: 'Dodaj koszt własny', iconSet: FontAwesome5, style: { marginBottom: 30 } },
+    { screen: 'FuelingScreen', icon: 'gas-pump', text: 'Historia tankowań', iconSet: FontAwesome5 },
+    { screen: 'RoadFeesScreen', icon: 'road', text: 'Historia opłat drogowych', iconSet: FontAwesome },
+    { screen: 'DriverCostsScreen', icon: 'money-bill', text: 'Historia kosztów własnych', iconSet: FontAwesome5 },
+];
 
 export const HomeScreen = () => {
     const [menuVisible, setMenuVisible] = useState(false);
@@ -29,42 +38,21 @@ export const HomeScreen = () => {
         });
     }, [navigation]);
 
-
     return (
         <View style={styles.container}>
             <StatusBar barStyle="light-content" backgroundColor="#E8364F" />
 
             <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 20 }}>Wybierz akcję:</Text>
 
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('AddFuelScreen')}>
-                <FontAwesome5 name="gas-pump" size={30} color="#fff" />
-                <Text style={styles.text}>Dodaj Tankowanie</Text>
-            </TouchableOpacity>
+            {
+                buttons.map(({ screen, icon, text, iconSet, style }, index) => (
+                    <TouchableOpacity key={index} style={[styles.button, style]} onPress={() => navigation.navigate(screen)}>
+                        {React.createElement(iconSet, { name: icon, size: 30, color: "#fff" })}
+                        <Text style={styles.text}>{text}</Text>
+                    </TouchableOpacity>
+                ))
+            }
 
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('AddRoadFeesScreen')}>
-                <FontAwesome name="road" size={30} color="#fff" />
-                <Text style={styles.text}>Dodaj koszt drogowy</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={[styles.button, { marginBottom: 30 }]} onPress={() => navigation.navigate('AddDriverCostsScreen')}>
-                <FontAwesome5 name="money-bill" size={30} color="#fff" />
-                <Text style={styles.text}>Dodaj koszt własny</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('FuelingScreen')}>
-                <FontAwesome5 name="gas-pump" size={30} color="#fff" />
-                <Text style={styles.text}>Historia tankowań</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('RoadFeesScreen')}>
-                <FontAwesome name="road" size={30} color="#fff" />
-                <Text style={styles.text}>Historia opłat drogowych</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('DriverCostsScreen')}>
-                <FontAwesome5 name="money-bill" size={30} color="#fff" />
-                <Text style={styles.text}>Historia kosztów własnych</Text>
-            </TouchableOpacity>
             <MenuModal menuVisible={menuVisible} setMenuVisible={setMenuVisible} />
         </View>
     );
@@ -84,7 +72,7 @@ const styles = StyleSheet.create({
         margin: 5,
         marginBottom: 10,
         borderRadius: 5,
-        width: '80%',
+        width: Dimensions.get('window').width * 0.8,
         shadowColor: "#000",
         shadowOffset: {
             width: 0,
@@ -101,4 +89,3 @@ const styles = StyleSheet.create({
         textAlign: 'center',
     },
 });
-
